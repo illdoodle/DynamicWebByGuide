0d3d0f7907e6c40584e60e4c5b6ffa64d37df36e
illdoodle <illdoodmail@gmail.com>
2023-08-15 13:38:38 +0300

    html, css, bootstrap, carousel and header fix

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.rtl.min.css" integrity="sha384-PRrgQVJ8NNHGieOA1grGdCTIt4h21CzJs6SnWH4YMQ6G5F5+IEzOHz67L4SQaF0o" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <title>My Blog</title>
 </head>
@@ -33,5 +34,42 @@
         </div>
     </div>
 </header>
+
+<div class="container">
+    <div class="row">
+        <h2 class="slider-title">Топ Публикации</h2>
+    </div>
+    <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/Tire.png" class="d-block w-100" alt="Tire.png">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><a href="">First slide label</a></h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/DoublePizdec.png" class="d-block w-100" alt="DoublePizdec.png">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><a href="">Second slide label</a></h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/Chasiki.png" class="d-block w-100" alt="Chasiki.png">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><a href="">Third slide label</a></h5>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+
 </body>
 </html>
