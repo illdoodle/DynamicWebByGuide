a769d52181233aca14cdad746a2a37d696190f41
illdoodle <illdoodmail@gmail.com>
2023-08-16 19:17:36 +0300

    html&css - main and footer

--- index.php
+++ index.php
@@ -71,5 +71,116 @@
     </div>
 </div>
 
+<div class="container">
+    <div class="content row">
+        <div class="main-content col-md-9 col-12">
+            <h2>Последние Публикации</h2>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="images/Chasiki.png" alt="Chasiki.png" class="img-thumbnail">
+                </div>
+                <div class="post-text col-12 col-md-8">
+                    <h2>
+                        <a href="">Заголовок</a>
+                    </h2>
+                    <i class="far fa-user">Имя Автора</i>
+                    <i class="far fa-calendar">01.01.1111</i>
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, repudiandae. 
+                        Suscipit adipisci quos recusandae dolor ex, quae in aspernatur incidunt.
+                    </p>
+                </div>
+            </div>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="images/Chasiki.png" alt="Chasiki.png" class="img-thumbnail">
+                </div>
+                <div class="post-text col-12 col-md-8">
+                    <h2>
+                        <a href="">Заголовок</a>
+                    </h2>
+                    <i class="far fa-user">Имя Автора</i>
+                    <i class="far fa-calendar">01.01.1111</i>
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, repudiandae. 
+                        Suscipit adipisci quos recusandae dolor ex, quae in aspernatur incidunt.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar col-md-3 col-12">
+            <div class="section search">
+                <h3>Search</h3>
+                <form action="index.php" method="post">
+                    <input type="text" name="search-term" class="text-input" placeholder="Search...">
+                </form>
+            </div>
+            <div class="section topics">
+                <h3>Topics</h3>
+                <ul>
+                    <li><a href="">Perms</a></li>
+                    <li><a href="">Quotes</a></li>
+                    <li><a href="">Fiction</a></li>
+                    <li><a href="">Biography</a></li>
+                    <li><a href="">Motivation</a></li>
+                    <li><a href="">Inspiration</a></li>
+                    <li><a href="">Life Lessons</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="footer container-fluid">
+    <div class="footer-content container">
+        <div class="row">
+            <div class="footer-section about col-md-4 col-12">
+                <h3 class="logo-text">Мой блог</h3>
+                <p>
+                    Мой блог - это блог сделаннный с целью обучения аудитории на платформе
+                    YouTube и заработка доволнительной кармы)).
+                </p>
+                <div class="contact">
+                    <i class="fas fa-phone"></i><span> 123-456-789</span> <br>
+                    <i class="fas fa-envelope"></i><span> info@myblog.com</span>
+                </div>
+                <div class="socials">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+
+            <div class="footer-section links col-md-4 col-12">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="">События</a></li>
+                    <li><a href="">Команда</a></li>
+                    <li><a href="">Упражнения</a></li>
+                    <li><a href="">Галерея</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-section contact-form col-md-4 col-12">
+                <h3>Контакты</h3>
+                <form action="index.html" method="post" class="contacts">
+                    <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+                    <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+                    <button type="submit" class="btn btn-big contact-btn">
+                        <i class="fas fa-envelope"></i>
+                        Отправить
+                    </button>
+                </form>
+            </div>
+
+        </div>
+
+        <div class="footer-bottom">
+            &copy; myblog.com | Designed by illdoodle. Maybe.
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
