f695e7a8844ef2f9caf7d94bc932fd62b7aa9792
illdoodle <illdoodmail@gmail.com>
2023-09-02 12:54:09 +0300

    selectAll, $params, limitone

--- app/database/connect.php
+++ app/database/connect.php
@@ -5,7 +5,7 @@ $db_name = 'learn-db';
 $db_user = 'root';
 $db_password = 'mysql';
 $charset = 'utf8';
-$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]; // Второй параметр отменяет дублирование в ассоциативном массиве.
 
 try {
     $pdo = new PDO("$driver:host=$host;dbname=$db_name;charset=$charset", $db_user, $db_password, $options);
--- app/database/database.php
+++ app/database/database.php
@@ -1,26 +1,51 @@
 <?php 
 require 'connect.php';
 
-function printValue($value){
+function printArr($value){
     echo '<pre>';
     print_r($value);
     echo '</pre>';
 }
 
-function selectAll($table){
-    global $pdo;
-    $sql = "SELECT * FROM $table";
-    $query = $pdo->prepare($sql);
-    $query->execute();
+function dbCheckError($query){
     $errInfo = $query->errorInfo();
-
     if($errInfo[0] !== PDO::ERR_NONE){
         echo $errInfo[2];
         exit();
     }
+}
+
+// Запрос на получение данных из одной таблицы.
+$params = [
+    'admin' => 0
+];
+function selectAll($table, $params = [], $limitOne){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+    if(!empty($params)){
+        $i = 0;
+        foreach($params as $key => $value){
+            if(!is_numeric($value)){
+                $value = "'" . $value . "'";
+            }
+            if($i === 0){
+                $sql = $sql . " WHERE $key = $value";
+            }else{
+                $sql = $sql . " AND $key = $value";
+            }
+            $i++;
+        }
+    }
+    if($limitOne){
+        $sql = $sql . " LIMIT 1";
+    }
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
     return $query->fetchAll(); //Либо fetch (таблица, а не вся база)
 }
 
-printValue(selectAll('users'));
+printArr(selectAll('users', $params, true));
 
 ?>
+<html lang="ru"></html>
