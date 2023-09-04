ad7ccfdd129fa69b8612cfd395b340417d877304
illdoodle <illdoodmail@gmail.com>
2023-09-04 14:17:30 +0300

    CRUD functions

--- app/database/database.php
+++ app/database/database.php
@@ -45,7 +45,81 @@ function selectAll($table, $params = [], $limitOne){
     return $query->fetchAll(); //Либо fetch (таблица, а не вся база)
 }
 
-printArr(selectAll('users', $params, true));
+// printArr(selectAll('users', $params, true));
+
+function resetId($table){
+    global $pdo;
+    $sql = "ALTER TABLE $table AUTO_INCREMENT = 1";
+    
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
+}
+resetId('users');
+
+$insertData = [
+    'admin' => '0',
+    'username' => 'aboba',
+    'email' => 'aboba@gmail.com',
+    'password' => 'abobapw'
+];
+
+function insert($table, $params){
+    global $pdo;
+    // Разбираем массив на ключ и значения.
+    $i = 0;
+    $col = '';
+    $mask = '';
+    foreach($params as $key => $value){
+        // Проверка, которая позволяет не ставит запятые в начале sql запроса (и обрамляет в кавычки).
+        if($i === 0){
+            $col = $col . "$key";
+            $mask = $mask . "'" . "$value" . "'";    
+        }else{
+            $col = $col . ", $key";
+            $mask = $mask . ", '" . "$value" . "'";   
+        }
+        $i++;
+    }
+    $sql = "INSERT IGNORE INTO $table ($col) VALUES ($mask)";
+    $query = $pdo->prepare($sql);
+    $query->execute($params);
+    dbCheckError($query);
+}
+// insert('users', $insertData);
+
+$updateData = [
+    'admin' => '1'
+];
+function update($table, $id, $params){
+    global $pdo;
+    // Разбираем массив на ключ и значения.
+    $i = 0;
+    $str = '';
+    foreach($params as $key => $value){
+        // Проверка, которая корректно ставит запятые в начале sql запроса (и обрамляет в кавычки).
+        if($i === 0){
+            $str = $str . $key . " = '" . $value . "'";
+        }else{
+            $str = $str . ", " . $key . " = '" . $value . "'";
+        }
+        $i++;
+    }
+    $sql = "UPDATE $table SET $str WHERE id = $id";
+    $query = $pdo->prepare($sql);
+    $query->execute($params);
+    dbCheckError($query);
+}
+// update('users', '3', $updateData);
+
+function delete($table, $id){
+    global $pdo;
+    $sql = "DELETE FROM $table WHERE id = $id";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
+}
+delete('users', '4')
 
 ?>
 <html lang="ru"></html>
